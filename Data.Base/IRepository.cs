ad4df466051c0060f555584b032547161e4f2f74
querbyte <querbyte@gmail.com>
2019-03-19 08:54:45 +0100

    #2

---
+++ Data.Base/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Data.Base
+{
+    public interface IRepository<T>
+    {
+        T Get(int id);
+
+        IEnumerable<T> GetAll();
+
+        IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
+    }
+}
